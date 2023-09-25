0a99b0facba995448c201374a9a08ff1f9716b42
amandayehh <amandayeh123@gmail.com>
2023-09-24 22:51:03 -0700

    stuff

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ let total = 100;
 for (let i = 0; i < total; i++) {
     let img = document.createElement('img');
     let randInt = getRandomInt(1, 18);
+    img.classList.add('fadeout');
+    // console.log("translate(" + getRandomInt(-1, 1) + "vw ," + getRandomInt(-1, 1) + "vw)")
+    // img.style.transform = "translate(" + getRandomInt(-4, 4) + "vw ," + getRandomInt(-4, 4) + "vw)";
+
     img.src = "bubbles/bubble" + randInt + ".png";
     img.id = "bubble" + i;
-    console.log(document.getElementById("bubbles"))
     document.getElementById("bubbles").appendChild(img);
 }
 
 document.addEventListener('mouseover', function (event) {
-    if (event.target.id) { // only log if it actually has an id
-        console.log(event.target.id);
+    if (event.target.id && event.target.tagName == "IMG") {
         event.target.classList.add('active');
+        event.target.classList.remove('fadeout');
+
+        event.target.classList.add('fadein');
+        event.target.style.transform = "translate(" + getRandomInt(-4, 4) + "vw ," + getRandomInt(-4, 4) + "vw)";
+        event.target.style.transform += "rotate(" + getRandomInt(-30, 30) + "deg)";
+
+        setTimeout(function () {
+            event.target.classList.remove('fadein');
+            event.target.classList.add('fadeout');
+        }, 1200);
+
         //get audio file & play
-        var audio = document.getElementsByClassName("audio")[getRandomInt(0, 6)];
+        var audio = document.getElementsByClassName("audio")[getRandomInt(0, 13)];
         audio.play();
 
         //change image source
